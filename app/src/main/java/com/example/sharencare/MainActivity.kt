c82548dbab71452de45a5e87c8adabadd9de8154
ShamikShafkat <shamik.shafkat@gmail.com>
2022-09-30 06:35:31 +0600

    Implementing MainActivity.kt for wroking of Bottom Navigation Bar

--- app/src/main/java/com/example/sharencare/MainActivity.kt
+++ app/src/main/java/com/example/sharencare/MainActivity.kt
@@ -3,18 +3,53 @@ package com.example.sharencare
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.sharencare.databinding.ActivityMainBinding
+import com.example.sharencare.fragment.Favorites_Fragment
+import com.example.sharencare.fragment.Music_Fragment
+import com.example.sharencare.fragment.Notifications_Fragment
+import com.example.sharencare.fragment.Place_Fragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var selectedFragment :Fragment ?= null
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_favourites -> {
+                selectedFragment = Favorites_Fragment()
+            }
+            R.id.nav_music -> {
+                selectedFragment = Music_Fragment()
+            }
+            R.id.nav_place -> {
+                selectedFragment = Place_Fragment()
+            }
+            R.id.nav_notifications -> {
+                selectedFragment = Notifications_Fragment()
+            }
+        }
+
+        if(selectedFragment!=null)
+        {
+            supportFragmentManager.beginTransaction().replace(R.id.frame_layout,selectedFragment!!).commit()
+            return@OnNavigationItemSelectedListener true
+        }
+
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView : BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout,Favorites_Fragment()).commit()
     }
 }
